dc3499ec3b6f24af45f0b9d57b39a8789dc28e3a
EscapeTheCode <nthabisengnkuna90@gmail.com>
2022-04-09 08:54:25 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+/**
+ * main entry
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+
+{
+	char alphabet;
+	
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+return (0);
+
