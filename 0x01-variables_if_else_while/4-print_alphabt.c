1738716539e9bef56acf3b0538ed2fd8ebe6ffe2
EscapeTheCode <nthabisengnkuna90@gmail.com>
2022-04-09 22:57:35 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,16 @@
 int main(void)
 
 {
+
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-if (alphabet == 'q')
-continue;
+	{
+	if (alphabet == 'q')
+	continue;
 
-else if (alphabet == 'e')
-continue;
+	else if (alphabet == 'e')
+	continue;
 
 putchar (alphabet);
 }
