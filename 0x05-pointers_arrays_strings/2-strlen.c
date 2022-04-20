b42b742325913198d7830b0044df5e604f25b91d
EscapeTheCode <nthabisengnkuna90@gmail.com>
2022-04-20 02:34:00 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of a string
+ * @s: A pointer to an int that will be changed or updated
+ *
+ * Return: void that means the answer is correct
+ */
+
+int _strlen(char *s)
+{
+	int p;
+
+	p = o;
+	while (s[p] != '\0')
+	{
+		p++;
+	}
+
+return (p);
+
+}
