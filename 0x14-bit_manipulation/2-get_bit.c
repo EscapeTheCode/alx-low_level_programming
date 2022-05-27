751f3528e029f04d49456efc4be501d8d207db41
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-05-27 06:56:48 +0200

    Update 2-get_bit.c
    My commit message on task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,28 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - A function that prints a binary number without % or /
- * @n: The number to be printed
- * Return: Nothing
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
  */
-void print_binary(unsigned long int n)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+    int bit_val;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
-	}
+    if (index > 63)
+        return (-1);
+
+    bit_val = (n >> index) & 1;
+
+    return (bit_val);
 }
