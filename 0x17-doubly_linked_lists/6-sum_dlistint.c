dca27e6d1a5afcf67fa703060a8d7cbd728f41dd
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-08 15:46:51 +0200

    Update 6-sum_dlistint.c
    My commit message on task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,24 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
- * @head: pointer to head of the list
- * Return: sum of all nodes or 0
- **/
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ *
+ * Return: The sum of all the data.
+ */
 int sum_dlistint(dlistint_t *head)
 {
-	long int sum;
-	dlistint_t *tmp;
+	int sum = 0;
 
-	sum = 0;
-	if (head == NULL)
-	return (sum);
-
-	tmp = head;
-	while (tmp)
+	while (head)
 	{
-	sum += tmp->n;
-	tmp = tmp->next;
+		sum += head->n;
+		head = head->next;
 	}
+
 	return (sum);
 }
