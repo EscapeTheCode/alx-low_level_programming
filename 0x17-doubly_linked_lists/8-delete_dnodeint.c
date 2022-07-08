6dd1411ce73437b2412709fb62904ec9c834f55c
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-08 15:49:21 +0200

    Update 8-delete_dnodeint.c
    My commit message on task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,42 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at given index of a list
- * @head: pointer to head of the list
- * @index: index to delete from, starting from 0
- * Return: 1 on success or -1 on failure
- **/
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int count;
-	dlistint_t *tmp;
+	dlistint_t *tmp = *head;
 
-	if (*head == NULL || head == NULL)
-	return (-1);
-	if (index == 0)
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		if (*head)
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
 			(*head)->prev = NULL;
-		free(tmp), tmp = NULL;
-		return (1);
 	}
-	count = 1, tmp = (*head)->next;
-	if (tmp)
+
+	else
 	{
-		while (tmp->next)
-		{
-			if (index == count)
-			{
-				tmp->prev->next = tmp->next;
-				tmp->next->prev = tmp->prev;
-				free(tmp), tmp = NULL;
-				return (1);
-			}
-			count++, tmp = tmp->next;
-		}
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
 	}
-	if (tmp && index == count)
-	{
-		tmp->prev->next = NULL;
-		free(tmp);
-		return (1);
-	}
-	return (-1);
+
+	free(tmp);
+	return (1);
 }
